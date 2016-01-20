d313b727c97a87f5383e74baedb9d129476e47ed
lylychen <lyly.chen@thinkpower.com.tw>
2016-01-20 10:36:04 +0800

    第一個Scenario顯示綠燈

--- PotterShoppingCart/PotterShoppingCart/CalculatorFeeService.cs
+++ PotterShoppingCart/PotterShoppingCart/CalculatorFeeService.cs
@@ -10,10 +10,46 @@ namespace PotterShoppingCart
     public class CalculatorFeeService
     {
 
+        private IShoppingCartDao _ShoppingCartDao;
+
+        public CalculatorFeeService(IShoppingCartDao ShoppingCartDao)
+        {
+            this._ShoppingCartDao = ShoppingCartDao;
+        }
+
         public int CalculatorFee()
         {
-            return 0;
+            var ShoppingCarts = _ShoppingCartDao.GetShoppingCart();
+
+            var ordersOfBook = ShoppingCarts.Where(x=>x.Quantity > 0).Sum(x => x.SellPrice * x.Quantity);
+
+            return ordersOfBook;
         }  
+    }
+
+
+    public class ShoppingCartDao : IShoppingCartDao
+    {
+        List<ShoppingCart> IShoppingCartDao.GetShoppingCart()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IShoppingCartDao
+    {
+        List<ShoppingCart> GetShoppingCart();
+    }
+
+    public class ShoppingCart
+    {
+        public int Episode { get; set; }
+
+        public string BookName { get; set; }
+
+        public int SellPrice { get; set; }
+
+        public int Quantity { get; set; }
     }
     
 }
--- PotterShoppingCart/PotterShoppingCartTests/CalculatorFeeServiceTests.cs
+++ PotterShoppingCart/PotterShoppingCartTests/CalculatorFeeServiceTests.cs
@@ -16,7 +16,8 @@ namespace PotterShoppingCart.Tests
         public void CalculatorFee_is_1_episode_1_quantity_Fee_Should_Be_100()
         {
             //arrange
-            var target = new CalculatorFeeService();
+            var stubDAO = new StubShoppingCartDao();
+            var target = new CalculatorFeeService(stubDAO);
 
             var expected = 100;
 
@@ -28,6 +29,21 @@ namespace PotterShoppingCart.Tests
 
         }        
         
+    }
+
+    public class StubShoppingCartDao : IShoppingCartDao
+    {
+        public List<ShoppingCart> GetShoppingCart()
+        {
+            return new List<ShoppingCart>()
+                {
+                    new ShoppingCart() { Episode=1 ,BookName="Harry Potter and the Sorcerer's Stone", SellPrice = 100, Quantity=1 },
+                    new ShoppingCart() { Episode=2 ,BookName="Harry Potter and the Chamber of Secrets", SellPrice = 100, Quantity=0 },
+                    new ShoppingCart() { Episode=3 ,BookName="Harry Potter and the Prisoner of Azkaban", SellPrice = 100, Quantity=0 },
+                    new ShoppingCart() { Episode=4 ,BookName="Harry Potter and the Goblet of Fire", SellPrice = 100, Quantity=0 },
+                    new ShoppingCart() { Episode=5 ,BookName="Harry Potter and the Order of the Phoenix", SellPrice = 100, Quantity=0 }
+                };
+        }
     }
     
 }
